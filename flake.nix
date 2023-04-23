63b0441948ce8ed6a26c3c26bd01167c65a3102d
Alexandria P <alxpettit@gmail.com>
2023-04-22 23:55:34 -0600

    working akasha flake!

--- flake.nix
+++ flake.nix
@@ -1,86 +1,31 @@
 {
   inputs = {
-    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
-    flake-utils.follows = "cargo2nix/flake-utils";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = inputs: with inputs;
+  outputs = { self, flake-utils, naersk, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
+        pkgs = (import nixpkgs) {
           inherit system;
-          overlays = [cargo2nix.overlays.default];
         };
 
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.61.0";
-          packageFun = import ./Cargo.nix;
-
-          packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++ [
-            (pkgs.rustBuilder.rustLib.makeOverride {
-              overrideAttrs = drv: {
-                propagatedNativeBuildInputs = drv.propagatedNativeBuildInputs or [ ] ++ [
-                  pkgs.alsaLib pkgs.pkg-config pkgs.libogg
-                ];
-              };
-            })
-          ];
-
-        };
-
-
+        naersk' = pkgs.callPackage naersk {};
 
       in rec {
-        packages = {
-          akasha = (rustPkgs.workspace.akasha {});
-          default = packages.akasha;
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = with pkgs; [ alsaLib libogg ];
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustc cargo ];
         };
       }
     );
 }
-
-# {
-#   description = "Headless audio recording application";
-
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
-#     flake-utils.url = "github:numtide/flake-utils";
-#     rust-overlay.url = "github:oxalica/rust-overlay";
-#   };
-
-#   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-#     flake-utils.lib.eachDefaultSystem (system:
-#       let
-#         pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };
-#         rustc = pkgs.rust-bin.stable.latest.default;
-#         cargo = pkgs.buildPackages.cargo;
-#       in
-#       rec {
-#         packages = {
-#           akasha = pkgs.stdenv.mkDerivation {
-#             name = "akasha";
-#             src = ./.;
-#             buildInputs = [ cargo rustc pkgs.alsaLib pkgs.pkg-config ];
-
-#             buildPhase = ''
-#               export CARGO_HOME=$(mktemp -d)
-#               cargo build --release
-#             '';
-
-#             installPhase = ''
-#               install -D -m755 target/release/akasha $out/bin/akasha
-#             '';
-
-#             meta = with pkgs.lib; {
-#               homepage = "https://github.com/alxpettit/akasha";
-#               description = "Headless audio recording application";
-#               license = licenses.agpl3Plus;
-#               platforms = platforms.all;
-#             };
-#           };
-#         };
-
-#         defaultPackage = packages.akasha;
-#       });
-# }
