9ceb28c6dd3e79075342979b228b26069546be22
Alexandria P <alxpettit@gmail.com>
2023-04-22 21:21:23 -0600

    try to fix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,43 @@
 {
-  description = "Akasha package";
+  description = "Headless audio recording application";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs.inputs.flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
-        inherit system;
-      };
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };
+        rustc = pkgs.rust-bin.stable.latest.default;
+        cargo = pkgs.buildPackages.cargo;
+      in
+      rec {
+        packages = {
+          akasha = pkgs.stdenv.mkDerivation {
+            name = "akasha";
+            src = ./.;
+            buildInputs = [ cargo rustc pkgs.alsaLib pkgs.pkg-config ];
 
-      pkgs.rustPlatform.buildRustPackage {
-        pname = "akasha";
-        version = "1.2.4";
-        cargoLock.lockFile = ./Cargo.lock;
-        src = pkgs.lib.cleanSource ./.;
-        nativeBuildInputs = [ pkgs.pkg-config ];
-        buildInputs = [ pkgs.alsa-lib ];
-      }
-    );
+            buildPhase = ''
+              cargo build --release
+            '';
+
+            installPhase = ''
+              install -D -m755 target/release/akasha $out/bin/akasha
+            '';
+
+            meta = with pkgs.lib; {
+              homepage = "https://github.com/alxpettit/akasha";
+              description = "Headless audio recording application";
+              license = licenses.agpl3Plus;
+              platforms = platforms.all;
+            };
+          };
+        };
+
+        defaultPackage = packages.akasha;
+      });
 }
